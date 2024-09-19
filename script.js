6dc3c7692cdb37eedfc4e15f61aaa4c2536dc9ab
drewc830 <drewc830@gmail.com>
2024-09-19 10:39:26 -0500

    added popup image

---
+++ script.js
@@ -0,0 +1,32 @@
+
+shown = false;
+
+function showImage(elemId, imgSrc)
+{
+    if(shown)
+    {
+        console.log("element is unclicked");
+        console.log("\tElemId:" + elemId);
+        const elem = document.getElementById(elemId);
+        while (elem.childElementCount > 0)
+        {
+        elem.removeChild(elem.lastChild);
+        }
+        shown=false;
+    }
+    else
+    {
+    console.log("element is clicked");
+    console.log("\tElemId:" + elemId + " Image Source:" + imgSrc);
+    const elem = document.getElementById(elemId);
+    const popImage = new Image(600,400);
+    popImage.src = imgSrc;
+    popImage.style.position = "absolute";
+    popImage.style.zIndex = "1";
+    elem.appendChild(popImage);
+    shown = true;
+    }
+    
+}
+
+
