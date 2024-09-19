7d27a496069c4e36bd81ddc02a92978a344fb845
drewc830 <drewc830@gmail.com>
2024-09-19 10:58:09 -0500

    peepeepoopoo

--- script.js
+++ script.js
@@ -19,15 +19,16 @@ function showImage(elemId, imgSrc)
     console.log("element is clicked");
     console.log("\tElemId:" + elemId + " Image Source:" + imgSrc);
     const elem = document.getElementById(elemId);
-    const popImage = new Image(600,400);
+    const popImage = new Image();
     popImage.src = imgSrc;
-    popImage.style.position = "absolute";
-    popImage.style.zIndex = "1";
+    popImage.style.width = "100%";
+    popImage.style.height = "auto";
     popImage.className = "popupImage";
     elem.appendChild(popImage);
     shown = true;
     }
     
 }
+
 
 
